22c070ecf3d93188376e3290afbe973108cdf250
Saiganesh Voona <127197826+SAIGANESH-0@users.noreply.github.com>
2023-12-30 08:04:55 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-     {count}
-    </div>
-  );
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
