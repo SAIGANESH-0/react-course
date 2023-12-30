ed1638cd65a218d05eeabac40c2af515dc1c369f
Saiganesh Voona <127197826+SAIGANESH-0@users.noreply.github.com>
2023-12-30 07:40:57 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+    <div>
+     {count}
+    </div>
   );
 }
 
